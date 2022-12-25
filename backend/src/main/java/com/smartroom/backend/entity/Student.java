85e7fdb27b8447a392b857bc02d3ec34e6a3145b
Sarthak1505 <msarthak1505@gmail.com>
2022-12-25 15:19:47 +0530

    added student personal details

--- backend/src/main/java/com/smartroom/backend/entity/Student.java
+++ backend/src/main/java/com/smartroom/backend/entity/Student.java
@@ -3,9 +3,7 @@ package com.smartroom.backend.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -30,21 +28,60 @@ public class Student {
 
     private String password = String.valueOf(new Random().nextInt(999999));
 
+    @NotNull
+    @NotEmpty
+    private String standard;
 
+    @NotNull
+    @NotEmpty
+    private String batch;
+
+    @NotNull
+    @NotEmpty
+    private String dob;
+
+    @NotNull
+    @NotEmpty
+    private String fullAddress;
+
+    @NotNull
+    @Min(10)
+    @Max(10)
+    private Long phone;
+
+    @NotNull
+    @NotEmpty
+    private String motherName;
+
+    @NotNull
+    @NotEmpty
+    private String fatherName;
+
+    @NotNull
+    @NotEmpty
     private StudentParameter studentParameter;
 
-    private HashMap<String,Integer> studentMarks;
+    @NotNull
+    @NotEmpty
+    private HashMap<String, Integer> studentMarks;
 
     private String role = "ROLE_STUDENT";
 
     public Student() {
     }
 
-    public Student(String studentId, String studentName, String email, String password, StudentParameter studentParameter, HashMap<String, Integer> studentMarks, String role) {
+    public Student(String studentId, String studentName, String email, String password, String standard, String batch, String dob, String fullAddress, Long phone, String motherName, String fatherName, StudentParameter studentParameter, HashMap<String, Integer> studentMarks, String role) {
         this.studentId = studentId;
         this.studentName = studentName;
         this.email = email;
         this.password = password;
+        this.standard = standard;
+        this.batch = batch;
+        this.dob = dob;
+        this.fullAddress = fullAddress;
+        this.phone = phone;
+        this.motherName = motherName;
+        this.fatherName = fatherName;
         this.studentParameter = studentParameter;
         this.studentMarks = studentMarks;
         this.role = role;
@@ -82,6 +119,62 @@ public class Student {
         this.password = password;
     }
 
+    public String getStandard() {
+        return standard;
+    }
+
+    public void setStandard(String standard) {
+        this.standard = standard;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
     public StudentParameter getStudentParameter() {
         return studentParameter;
     }
@@ -113,6 +206,13 @@ public class Student {
                 ", studentName='" + studentName + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", standard='" + standard + '\'' +
+                ", batch='" + batch + '\'' +
+                ", dob='" + dob + '\'' +
+                ", fullAddress='" + fullAddress + '\'' +
+                ", phone=" + phone +
+                ", motherName='" + motherName + '\'' +
+                ", fatherName='" + fatherName + '\'' +
                 ", studentParameter=" + studentParameter +
                 ", studentMarks=" + studentMarks +
                 ", role='" + role + '\'' +
